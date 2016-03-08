0b5ca17a252ea9cdc194f1f07c4ac244df88e27b
wenbin <756412858@qq.com>
2016-03-08 16:12:25 +0800

    今天吃什么页面

--- gen/com/zhangshun/keep_in_good_health/R.java
+++ gen/com/zhangshun/keep_in_good_health/R.java
@@ -25,13 +25,20 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080008;
         public static final int dietary=0x7f080001;
+        public static final int intent_breakfast=0x7f080004;
+        public static final int intent_dinner=0x7f080007;
+        public static final int intent_lunch=0x7f080005;
+        public static final int intent_lunch_two=0x7f080006;
         public static final int recommended=0x7f080000;
+        public static final int scroll_list=0x7f080003;
+        public static final int today_eat_return=0x7f080002;
     }
     public static final class layout {
         public static final int food_name=0x7f030000;
         public static final int home_page=0x7f030001;
+        public static final int today_eat=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -39,10 +46,17 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int breakfast=0x7f050007;
+        public static final int breakfastcontext=0x7f05000a;
+        public static final int dinner=0x7f050009;
+        public static final int dinnercontext=0x7f05000c;
+        public static final int eattitle=0x7f050006;
         public static final int food_name=0x7f050003;
         public static final int hello_world=0x7f050002;
         public static final int home_page_dietary=0x7f050005;
         public static final int home_page_recommended=0x7f050004;
+        public static final int lunch=0x7f050008;
+        public static final int lunchcontext=0x7f05000b;
     }
     public static final class style {
         /** 
