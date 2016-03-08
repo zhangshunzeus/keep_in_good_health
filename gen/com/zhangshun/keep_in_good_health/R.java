a49b003c36fb32b326af9ad75fd6a074fa7bf10e
RuffianZhang <tangmenshun@foxmail.com>
2016-03-08 14:40:16 +0800

    Update the test again

--- gen/com/zhangshun/keep_in_good_health/R.java
+++ gen/com/zhangshun/keep_in_good_health/R.java
@@ -30,7 +30,8 @@ public final class R {
         public static final int recommended=0x7f080000;
     }
     public static final class layout {
-        public static final int home_page=0x7f030000;
+        public static final int food_name=0x7f030000;
+        public static final int home_page=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -38,9 +39,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int food_name=0x7f050003;
         public static final int hello_world=0x7f050002;
-        public static final int home_page_dietary=0x7f050004;
-        public static final int home_page_recommended=0x7f050003;
+        public static final int home_page_dietary=0x7f050005;
+        public static final int home_page_recommended=0x7f050004;
     }
     public static final class style {
         /** 
