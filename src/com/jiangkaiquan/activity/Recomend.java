cdd87c1e97825508d7037e77ab875dca73fe6431
aopeners <JiangKaiquan@Outlook.com>
2016-03-08 17:41:38 +0800

    recmend writen

---
+++ src/com/jiangkaiquan/activity/Recomend.java
@@ -0,0 +1,24 @@
+package com.jiangkaiquan.activity;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+
+import com.zhangshun.keep_in_good_health.R;
+
+/**
+ * Created by jkqme on 2016/3/8.
+ *
+ */
+public class Recomend extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recomend_indext_2);
+    }
+}
