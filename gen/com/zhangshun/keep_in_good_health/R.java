964662fbcecbe3f09fa1381810cde06e7bc02c14
RuffianZhang <tangmenshun@foxmail.com>
2016-03-09 08:53:21 +0800

    Recommend the page upload the merger

--- gen/com/zhangshun/keep_in_good_health/R.java
+++ gen/com/zhangshun/keep_in_good_health/R.java
@@ -25,13 +25,29 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080010;
         public static final int dietary=0x7f080001;
+        public static final int recomend_bttom_1_tx=0x7f08000d;
+        public static final int recomend_bttom_2_tx=0x7f08000e;
+        public static final int recomend_bttom_3_tx=0x7f08000f;
+        public static final int recomend_children_img=0x7f080002;
+        public static final int recomend_flish=0x7f080007;
+        public static final int recomend_flish_gl=0x7f080008;
+        public static final int recomend_food_tx=0x7f080004;
+        public static final int recomend_fruit=0x7f080005;
+        public static final int recomend_fruit_gl=0x7f080006;
+        public static final int recomend_recomend_tx=0x7f080003;
+        public static final int recomend_tea_gl=0x7f08000c;
+        public static final int recomend_tea_tx=0x7f08000b;
+        public static final int recomend_vegable_gl=0x7f08000a;
+        public static final int recomend_vegtable_tx=0x7f080009;
         public static final int recommended=0x7f080000;
     }
     public static final class layout {
         public static final int food_name=0x7f030000;
         public static final int home_page=0x7f030001;
+        public static final int recomend_children=0x7f030002;
+        public static final int recomend_indext_2=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
