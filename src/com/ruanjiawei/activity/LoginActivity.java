84d0818830cfa95a4c6a57685648b8c7fda32a37
ruanjiawei <297216624@qq.com>
2016-03-10 15:18:36 +0800

    登录跳注册

--- src/com/ruanjiawei/activity/LoginActivity.java
+++ src/com/ruanjiawei/activity/LoginActivity.java
@@ -3,13 +3,38 @@ package com.ruanjiawei.activity;
 import com.zhangshun.keep_in_good_health.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
+	Button login_register;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+		login_register = (Button) findViewById(R.id.login_register);
+		login_register.setOnClickListener(l);
 	}
+
+	OnClickListener l = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			switch (v.getId()) {
+			case R.id.login_register:
+				Intent intent = new Intent(LoginActivity.this, RegiserActivity.class);
+				startActivity(intent);
+				break;
+
+			default:
+				break;
+			}
+		}
+	};
 }
