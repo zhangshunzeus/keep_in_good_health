daf0b5c389c54fd3c460e56edfed9fa6ba99e56d
aopeners <JiangKaiquan@Outlook.com>
2016-03-15 15:36:12 +0800

    图片适配

--- src/com/jiangkaiquan/activity/MyFriend1.java
+++ src/com/jiangkaiquan/activity/MyFriend1.java
@@ -1,20 +1,27 @@
 package com.jiangkaiquan.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.jiangkaiquan.activity.adapter.MyFriend1Ad;
 import com.zhangshun.keep_in_good_health.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
 
 import com.jiangkaiquan.activity.adapter.RecomendAdapter;
@@ -30,9 +37,20 @@ public class MyFriend1 extends Activity {
     private TextView friend;
     private TextView social;
     private PopupWindow pwind;
+
     private ListView listview;
+    private ListView listView2;
     private MyFriend1Ad adpter;
-    private ArrayList<Object>list;
+    private MyFriend1Ad adpter2;
+    private ArrayList<HashMap<String,Object>> list;
+    private ArrayList<HashMap<String,Object>> list2;
+
+    private RelativeLayout layout;
+    private AlertDialog dialog;
+    private AlertDialog.Builder builder;
+    WindowManager.LayoutParams lp;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,12 +59,18 @@ public class MyFriend1 extends Activity {
         add = (ImageView) findViewById(R.id.myfriend1_add_img);
         friend = (TextView) findViewById(R.id.myfriend1_friend_tx);
         social = (TextView) findViewById(R.id.myfriend1_social_tx);
-
-        getDate();
-        listview=(ListView)findViewById(R.id.myfrend1_lv);
-        adpter=new MyFriend1Ad(list,this);
-        Log.i("onCreate","listview");
+        layout = (RelativeLayout) findViewById(R.id.friend1_rl);
+        // listview 准备
+        getFriendDate();
+        getSocialDate();
+        listview = (ListView) findViewById(R.id.myfrend1_lv);
+        listView2=(ListView)findViewById(R.id.myfrend1_1v2);
+        adpter = new MyFriend1Ad(list, this);
+        adpter2=new MyFriend1Ad(list2,this);
+        Log.i("onCreate", "listview");
         listview.setAdapter(adpter);
+        listView2.setAdapter(adpter2);
+
         back.setOnClickListener(listener);
         add.setOnClickListener(listener);
         friend.setOnClickListener(listener);
@@ -60,22 +84,26 @@ public class MyFriend1 extends Activity {
         @Override
         public void onClick(View view) {
             //dissmis popuwind
-            if (pwind!=null&&pwind.isShowing()) {
+            if (pwind != null && pwind.isShowing()) {
                 pwind.dismiss();
             }
             switch (view.getId()) {
                 case R.id.myfriend1_back_img:
                     break;
                 case R.id.myfriend1_add_img:
-                    displayPopuwind();
+                    setDiolog();
                     break;
                 case R.id.myfriend1_friend_tx:
-                    friend.setBackgroundResource(R.drawable.freind);
-                    social.setBackgroundColor(255255255);
+                    friend.setTextColor(getResources().getColor(R.color.friendbt_change));
+                    social.setTextColor(getResources().getColor(R.color.frendbt_no_change));
+                    listview.setVisibility(View.VISIBLE);
+                    listView2.setVisibility(View.GONE);
                     break;
                 case R.id.myfriend1_social_tx:
-                    social.setBackgroundResource(R.drawable.social);
-                    friend.setBackgroundColor(255255255);
+                    social.setTextColor(getResources().getColor(R.color.friendbt_change));
+                    friend.setTextColor(getResources().getColor(R.color.frendbt_no_change));
+                    listView2.setVisibility(View.VISIBLE);
+                    listview.setVisibility(View.GONE);
                     break;
                 case R.id.friend_swap_tx:
                     break;
@@ -105,25 +133,85 @@ public class MyFriend1 extends Activity {
             pwind.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
             pwind.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
         }
-        pwind.showAsDropDown(add);
+        pwind.showAsDropDown(layout);
+
         pwind.setOnDismissListener(ondismis);
     }
+
     //不显示接口
-    private PopupWindow.OnDismissListener ondismis=new PopupWindow.OnDismissListener() {
+    private PopupWindow.OnDismissListener ondismis = new PopupWindow.OnDismissListener() {
         @Override
         public void onDismiss() {
 
         }
     };
-    private void getDate(){
-        list=new ArrayList<Object>();
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
+
+    //设置diolog
+    private void setDiolog() {
+        if (dialog == null) {
+            //dialog 弹出窗试图
+            int id[] = {R.id.friend_swap_tx, R.id.friend_add_tx, R.id.friend_creat_social_tx};
+            View view = LayoutInflater.from(this).inflate(R.layout.jiang_popwind_my_friend, null);
+            TextView text;
+            for (int i = 0; i < id.length; i++) {
+                text = (TextView) view.findViewById(id[i]);
+                text.setSelected(true);
+                text.setOnClickListener(listener);
+            }
+            //dialog位置设置
+            int w = layout.getWidth();
+            int h = layout.getHeight();
+            builder = new AlertDialog.Builder(this);
+            builder.setView(view);
+            dialog = builder.create();
+            //  dialog.setContentView(R.layout.jiang_popwind_my_friend);
+
+            //获得当前窗体
+            Window window = dialog.getWindow();
+
+            //重新设置
+           lp = window.getAttributes();
+            window.setGravity(Gravity.RIGHT | Gravity.TOP);
+            lp.x = w - lp.x; // 新位置X坐标
+            lp.y = h; // 新位置Y坐标
+            lp.width = ViewGroup.LayoutParams.WRAP_CONTENT; // 宽度
+            lp.height = ViewGroup.LayoutParams.WRAP_CONTENT; // 高度
+            //lp.alpha = 0.7f; // 透明度
+
+            // dialog.onWindowAttributesChanged(lp);
+            //(当Window的Attributes改变时系统会调用此函数)
+            window.setAttributes(lp);
+        }
+        //builder.notify();
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
+    }
+    //我的好友数据
+    private void getFriendDate() {
+         HashMap<String,Object>map;
+        list = new ArrayList<HashMap<String,Object>>();
+
+        int imgId[]={R.drawable.friend1,R.drawable.friend2,R.drawable.friend3,R.drawable.friend4,R.drawable.friend5,R.drawable.friend6,R.drawable.friend7,R.drawable.friend8};
+        String text[]={"重我最帅","fdsa","我来拯救地球","美女","较比手好用","哈","天王","着了"};
+       for(int i=0;i<imgId.length;i++){
+           map=new HashMap<String, Object>();
+           map.put("img",imgId[i]);
+           map.put("text",text[i]);
+           list.add(map);
+       }
+
+    }
+    //我的群数据
+    private void getSocialDate() {
+        HashMap<String,Object>map;
+        list2 = new ArrayList<HashMap<String,Object>>();
+        int imgId[]={R.drawable.social1,R.drawable.social2,R.drawable.social3,R.drawable.social6};
+        String text[]={"健康","美丽","运动","90后"};
+        for(int i=0;i<imgId.length;i++){
+            map=new HashMap<String, Object>();
+            map.put("img",imgId[i]);
+            map.put("text",text[i]);
+            list2.add(map);
+        }
     }
 }
--- src/com/jiangkaiquan/activity/adapter/MyFriend1Ad.java
+++ src/com/jiangkaiquan/activity/adapter/MyFriend1Ad.java
@@ -6,22 +6,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.zhangshun.keep_in_good_health.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by jkqme on 2016/3/14.
  */
 public class MyFriend1Ad extends BaseAdapter {
-    ArrayList<Object>list;
+    ArrayList<HashMap<String,Object>>list;
     Context context;
     LayoutInflater inflater;
     public MyFriend1Ad() {
 
     }
-    public MyFriend1Ad(ArrayList<Object>list,
+    public MyFriend1Ad(ArrayList<HashMap<String,Object>>list,
             Context context) {
         this.context=context;
         this.list=list;
@@ -46,12 +49,26 @@ public class MyFriend1Ad extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+        Holder holder;
         Log.i("getView",getCount()+"");
        if(view==null){
            Log.i("getView","adpter==null");
-           view=inflater.inflate(R.layout.recomend_children,null);
-       }else {   Log.i("getView","adpter==null");}
+           holder=new Holder();
+           view=inflater.inflate(R.layout.friend_child,null);
+          holder.textView=(TextView)view.findViewById(R.id.friend_child_tx);
+           holder.img=(ImageView)view.findViewById(R.id.friend_child_img);
+            view.setTag(holder);
+       }else {
+           holder=(Holder)view.getTag();
+           Log.i("getView","adpter==null");}
+        holder.img.setImageDrawable(view.getResources().getDrawable((Integer)list.get(i).get("img")));
+        holder.textView.setText((String)list.get(i).get("text"));
+
 
     return view;
+    }
+    class Holder{
+        ImageView img;
+        TextView textView;
     }
 }
