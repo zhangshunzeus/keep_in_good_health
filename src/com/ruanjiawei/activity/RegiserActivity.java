96aa9e6fe4a8969cab1f6241381b46d4afbdb19b
zhangshunzeus <zhangshunzeus@163.com>
2016-03-21 11:39:47 +0800

    删除 import android.view.view.OnClickListener; 这行代码

--- src/com/ruanjiawei/activity/RegiserActivity.java
+++ src/com/ruanjiawei/activity/RegiserActivity.java
@@ -15,7 +15,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View.OnClickListener;
 import android.view.View;
-import android.view.View.OnClickListener;
+
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
