87230530ab64d98b687f88a28eb776d8ba7a84ca
aopeners <JiangKaiquan@Outlook.com>
2016-04-05 09:06:58 +0800

    myFriend 添加功能完善

--- src/com/jiangkaiquan/activity/adapter/NetUrl.java
+++ src/com/jiangkaiquan/activity/adapter/NetUrl.java
@@ -1,5 +1,8 @@
 package com.jiangkaiquan.activity.adapter;
 
 public interface NetUrl {
-	public String friend="/index.php/Home/MyFriend/";
+	public String friend = "/index.php/Home/MyFriend/";
+	public String ip1 = "http://192.168.11.251";
+	// 家用
+	public String ip2 = "http://192.168.1.109";
 }
